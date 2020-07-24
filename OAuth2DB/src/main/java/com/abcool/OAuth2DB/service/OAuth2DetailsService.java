9dffba664fd6a37a751a38ed97ae35002ebf11c9
Arvind Bakshi <arvindbakshicool@gmail.com>
2020-07-25 00:39:43 +0530

    Added BCrypt encryption to client secret

--- OAuth2DB/src/main/java/com/abcool/OAuth2DB/service/OAuth2DetailsService.java
+++ OAuth2DB/src/main/java/com/abcool/OAuth2DB/service/OAuth2DetailsService.java
@@ -1,6 +1,9 @@
 package com.abcool.OAuth2DB.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.abcool.OAuth2DB.dto.OAuth2DetailsRequestDTO;
@@ -11,8 +14,17 @@ import com.abcool.OAuth2DB.repository.OAuth2DetailsRepository;
 @Service
 public class OAuth2DetailsService implements IOAuth2Details{
 	
-	@Autowired OAuth2DetailsRepository repo;
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired 
+	OAuth2DetailsRepository repo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public OAuth2DetailsResponseDTO createClientDetails(OAuth2DetailsRequestDTO requestDTO) {
 		OAuth2DetailsResponseDTO response = new OAuth2DetailsResponseDTO();
 		if(requestDTO.getClient_id()==null || requestDTO.getClient_secret()==null) {
@@ -20,7 +32,7 @@ public class OAuth2DetailsService implements IOAuth2Details{
 		}else {
 		OAuthClientDetails domain = new OAuthClientDetails();
 		domain.setClient_id(requestDTO.getClient_id());
-		domain.setClient_secret(requestDTO.getClient_secret());
+		domain.setClient_secret(passwordEncoder.encode(requestDTO.getClient_secret()));
 		domain.setWeb_server_redirect_uri(requestDTO.getWeb_server_redirect_uri());
 		domain.setScope(requestDTO.getScope());
 		domain.setAccess_token_validity(requestDTO.getAccess_token_validity());
@@ -38,4 +50,6 @@ public class OAuth2DetailsService implements IOAuth2Details{
 		}
 		return response;
 	}
+	
+	
 }
