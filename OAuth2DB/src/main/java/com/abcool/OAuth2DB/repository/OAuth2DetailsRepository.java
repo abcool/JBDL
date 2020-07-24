1e17e547a45efd014a92c0a0a7c01b0ae4d7942b
Arvind Bakshi <arvindbakshicool@gmail.com>
2020-07-24 14:26:24 +0530

    Fixed Integer to String in OAuth2DetailsRepository

--- OAuth2DB/src/main/java/com/abcool/OAuth2DB/repository/OAuth2DetailsRepository.java
+++ OAuth2DB/src/main/java/com/abcool/OAuth2DB/repository/OAuth2DetailsRepository.java
@@ -3,6 +3,6 @@ package com.abcool.OAuth2DB.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.abcool.OAuth2DB.entity.OAuthClientDetails;
-public interface OAuth2DetailsRepository extends JpaRepository<OAuthClientDetails, Integer>{
+public interface OAuth2DetailsRepository extends JpaRepository<OAuthClientDetails, String>{
 
 }
