cdc974933f189a918c9b808aaa8cbe7ac10844f5
Arvind Bakshi <arvindbakshicool@gmail.com>
2020-07-25 19:33:32 +0530

    modified application.properties

--- OAuth2DB/src/main/java/com/abcool/OAuth2DB/OAuth2DbApplication.java
+++ OAuth2DB/src/main/java/com/abcool/OAuth2DB/OAuth2DbApplication.java
@@ -2,8 +2,10 @@ package com.abcool.OAuth2DB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class OAuth2DbApplication {
 
 	public static void main(String[] args) {
